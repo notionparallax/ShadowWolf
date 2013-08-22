89e91850ed9f074e4e1001d19656679c92a1369c
vagrant <vagrant@precise32.(none)>
2013-08-22 06:25:14 +0000

    ran $ rails new ShadowWolf --skip-active-record --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShadowWolf::Application.config.secret_key_base = 'f4785077f4d87c381e0bb91875b30fb7761f31135d0fdce9ea462ff40f614b3a90a56b6b56078501b28965d97475214ee673a043dde07209dd551e1faf67d1ea'
