002fceb240582a7559aacb7f83a031a8167b99b7
David Wilcox <dave@davidgwilcox.com>
2013-09-19 01:21:46 +0000

    added comments to gen_data script

--- server/bin/gen_data.rb
+++ server/bin/gen_data.rb
@@ -1,9 +1,12 @@
+# Commands to be passed to the scaffold generator
 models = <<-RUBY.split("\n")
 person
 person_name first:string last:string
 employee
 qualification name:string organisation:string
 RUBY
+
+# Commands to be passed to the relation generator
 relations = <<-RUBY.split("\n")
 person_name   --parent-class=person   --relation-type=embeds_one  --class_synonym=name
 employee      --parent-class=person   --relation-type=embeds_one
