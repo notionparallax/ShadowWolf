10f03480949e312831e3f71b118121f68b1bb90e
David Wilcox <dave@davidgwilcox.com>
2013-09-26 04:24:52 +0000

    changed gen_data script to have names and photos

--- server/bin/gen_data.rb
+++ server/bin/gen_data.rb
@@ -1,16 +1,16 @@
 # Commands to be passed to the scaffold generator
 models = <<-RUBY.split("\n")
 person
-person_name first:string last:string
+person_name first:string last:string preferred_first:string preferred_last:string
 employee
-qualification name:string organisation:string
+photos fun:string bw:string colour:string
 RUBY
 
 # Commands to be passed to the relation generator
 relations = <<-RUBY.split("\n")
 person_name   --parent-class=person   --relation-type=embeds_one  --class_synonym=name
 employee      --parent-class=person   --relation-type=embeds_one
-qualification --parent-class=employee --relation-type=embeds_many
+photos        --parent-class=employee --relation-type=embeds_one
 RUBY
 
 models.each do |model|
@@ -19,5 +19,3 @@ end
 relations.each do |rel|
   puts %x(rails g relation #{rel})
 end
-
-puts "\nAnd don't forget to tell app/views/people/{show,index}.json.jbuilder to point to app/views/people_person.json.jbuilder!"
