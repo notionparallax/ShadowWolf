52c4728043a6753ad4cfc22936b60c04225cebdf
David Wilcox <dave@davidgwilcox.com>
2013-10-04 05:13:01 +0000

    display box now transcludes

--- client/app/scripts/directives/display_box.js
+++ client/app/scripts/directives/display_box.js
@@ -1,26 +1,11 @@
 "use strict";
 angular.module('ShadowWolf')
 .directive("displayBox", function() {
-    var editorTemplate = 
-        '<div class="demo col-xs-6 col-sm-4 col-md-4 col-lg-3">                   '+  //TODO pass these in as an arg
-        '  <img src="http://placekitten.com/g/5/5" class="square">                '+  //TODO find a way around this
-        '  <div class="info-box"                                                  '+
-        '       ng-mouseenter="setDrawerState(\'drawer-out\')"                    '+
-        '       ng-mouseleave="setDrawerState(\'drawer-in\')"                     '+
-        '       ng-click     ="setDrawerState(\'click\')">                        '+
-        '    <img src="{{imgSrc}}" class="box-image">                             '+//http://placekitten.com/g/200/200
-        '    <div class="details {{state}}">                                      '+
-        '      <h1 class="title title-{{state}}">{{titleText}}</h1>               '+
-        '      <p><a class="btn btn-info info-button" href="{{moreLink}}">more</a> '+
-        '        {{bodyText}}</p>                                                 '+
-        '    </div>                                                               '+
-        '  </div>                                                                 '+
-        '</div>                                                                   ';
-
     return {
+        transclude: true,
         restrict: "E",
         replace: true,
-        template: editorTemplate,
+        templateUrl: 'scripts/directives/display_box.html',
         scope: {
           titleText: "@",
           bodyText:  "@",
