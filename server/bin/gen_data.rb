ac3d4b59e68c52b6928baa8b1cc2232534db4f28
David Wilcox <dave@davidgwilcox.com>
2013-10-04 05:32:28 +0000

    added contacts and mobiles to the gen script

--- server/bin/gen_data.rb
+++ server/bin/gen_data.rb
@@ -1,18 +1,22 @@
 # Commands to be passed to the scaffold generator
+#person
+#person_name first:string last:string preferred_first:string preferred_last:string
+#employee
+#photo fun:string bw:string colour:string
+#condition name:string start_date:date end_date:date
 models = <<-RUBY.split("\n")
-person
-person_name first:string last:string preferred_first:string preferred_last:string
-employee
-photo fun:string bw:string colour:string
-condition name:string start_date:date end_date:date
+contact extension:string studio:string
+mobile number:string is_bvn:boolean distribute:boolean
 RUBY
 
 # Commands to be passed to the relation generator
+#person_name   --parent-class=person   --relation-type=embeds_one  --class_synonym=name
+#employee      --parent-class=person   --relation-type=embeds_one
+#photo         --parent-class=employee --relation-type=embeds_one
+#condition     --parent-class=employee --relation-type=embeds_many
 relations = <<-RUBY.split("\n")
-person_name   --parent-class=person   --relation-type=embeds_one  --class_synonym=name
-employee      --parent-class=person   --relation-type=embeds_one
-photo         --parent-class=employee --relation-type=embeds_one
-condition     --parent-class=employee --relation-type=embeds_many
+contact       --parent-class=employee --relation-type=embeds_one
+mobile        --parent-class=contact  --relation-type=embeds_one
 RUBY
 
 models.each do |model|
