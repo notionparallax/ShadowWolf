901c8affd28d48da18cee5dda399dfc27a0bb168
David Wilcox <dave@davidgwilcox.com>
2013-12-10 03:32:44 +0000

    closes #77 and closes #78

--- client/app/scripts/controllers/people/index.js
+++ client/app/scripts/controllers/people/index.js
@@ -16,14 +16,14 @@ function($scope, People) {
       // Split the query by spaces into terms
       var queryTerms = query.split(' '),
           matchAgainst =
-          [ (person.name.first || "")
-          , (person.name.last  || "")
+          [ (person.name.preferred_first || "")
+          , (person.name.preferred_last  || "")
           , (person.employee.contact.studio || "")
           ];
       // Then we OR each term against all the fields and AND all the terms.
       return queryTerms.map(function(term) {
           return matchAgainst.map(function(field) {
-            return field.match(term);
+            return field.toLowerCase().match(term.toLowerCase());
           }).reduce(function(a,b){return a || b;});
         }).reduce(function(a,b){return a && b;});
     };
