2e0483a6e6827cd4bc21dd0d1a8533a0b05bd52e
Ben Doherty <ben@notionparallax.co.uk>
2013-12-19 02:52:49 +0000

    added a spec for person_model against condtions sharing start_dates

--- server/spec/models/person_spec.rb
+++ server/spec/models/person_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'conditions' do
+  	it 'should not allow two conditions with the same start date' do
+      person = FactoryGirl.create :person, condtion: []
+      2.times { person.condtions << Condition.new name: 'Active', start_date: 0.day.ago }
+      person.save.should eq(false)
+  	end
+  end
 end
+
