5f5137e1ad70f130f8583cb4bb813fff7aedd437
Ben Doherty <ben@notionparallax.co.uk>
2014-01-07 04:16:00 +0000

    make new person helper script #94

---
+++ server/new-person-helper.rb
@@ -0,0 +1,26 @@
+p = Person.new
+
+#p.name.prefix = ""
+#p.name.first = ""
+#p.name.middle = ""
+#p.name.last = ""
+#p.name.suffix = "" 
+#p.name.preferred_first = ""
+#p.name.preferred_last = ""
+
+#p.employee.photo.bw = "" 
+#p.employee.photo.colour = "" 
+#p.employee.photo.fun = ""
+
+#p.employee.position = ""
+#p.employee.login = ""
+
+#p.employee.contact.extension = ""
+#p.employee.contact.studio = ""
+#p.employee.contact.social.instagram = ""
+#p.employee.contact.social.twitter = ""
+#p.employee.contact.social.github = ""
+
+p.save
+
+puts "person creation done"
