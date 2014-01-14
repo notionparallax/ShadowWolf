b6929d502e3d9bd90dbdc963a0a632124dead87c
David Wilcox <dave@davidgwilcox.com>
2014-01-14 08:41:05 +0000

    somehow the api_key model was incomplete. fixed

--- server/app/models/api_key.rb
+++ server/app/models/api_key.rb
@@ -1,11 +1,22 @@
 class ApiKey
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  before_create :generate_access_token
+
   field :access_token, type: String
 
-  has_one :person
-  validates_presence_of :person
+  belongs_to :person
+  #validates_presence_of :person
 
   def expired?
     self.created_at + 1.day < DateTime.now
+  end
+
+  private
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while ApiKey.where( access_token: access_token ).first
   end
 end
