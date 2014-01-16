ecc610d1eb612270908a942ebbfaea719aee6c84
David Wilcox <dave@davidgwilcox.com>
2014-01-16 02:35:39 +0000

    added an apiKey access_token to the redirect url taken from the params

--- server/app/controllers/people/omniauth_callbacks_controller.rb
+++ server/app/controllers/people/omniauth_callbacks_controller.rb
@@ -1,10 +1,12 @@
+require 'uri'
 class People::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def ldap
     @person = Person.all.find_all { |p| p.employee.login == auth_login }.first
     if @person#.persisted?
       #sign_in_and_redirect @person, :event => :authentication
+      apiKey = ApiKey.create person: @person
       sign_in @person
-      redirect_to '/'
+      redirect_to  add_param(params[:redirect_uri], 'access_token', apiKey.access_token)
       set_flash_message(:notice, :success, :kind => "LDAP") if is_navigational_format?
     else
       session["devise.ldap_data"] = auth_login
@@ -16,6 +18,13 @@ class People::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
     
   protected
+  def add_param(url, param_name, param_value)
+    uri = URI(url)
+    params = URI.decode_www_form(uri.query || []) << [param_name, param_value]
+    uri.query = URI.encode_www_form(params)
+    uri.to_s
+  end
+
   def auth_hash
     request.env['omniauth.auth']
   end
