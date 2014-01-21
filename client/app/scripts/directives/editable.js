10153cbf78b1f30da323238e83224caec017df18
David Wilcox <dave@davidgwilcox.com>
2014-01-21 03:03:22 +0000

    minor changes to the editable directive. It still doesn't do anything yet.

--- client/app/scripts/directives/editable.js
+++ client/app/scripts/directives/editable.js
@@ -2,22 +2,25 @@
 
 angular.module("ShadowWolf")
 .directive("editable", function() {
+  var editDisabled = false;
   return {
     restrict: "E",
     replace: true,
     templateUrl: 'scripts/directives/editable.html',
     scope: {
       label: "@",
-      value: "@" 
+      value: "@",
+      name:  "@"
     },
     link: function (scope, element, attrs) { 
-     
+      if (attrs['editDisabled']) editDisabled = true;
     },
     controller: function($scope) {
       $scope.editableValue= $scope.value;
       $scope.editorEnabled= false;
 
       $scope.enableEditor = function() {
+        if (editDisabled) return;
         $scope.editorEnabled = true;
         $scope.editableValue = $scope.value;
       };
@@ -27,6 +30,7 @@ angular.module("ShadowWolf")
       };
 
       $scope.save = function() {
+        console.log('saving field: ' + $scope.label + ' as ' + $scope.value);
         $scope.value = $scope.editableValue;
         $scope.disableEditor();
       };
