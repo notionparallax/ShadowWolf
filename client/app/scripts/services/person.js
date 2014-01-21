fd8e353a0e04703457e853a8edc9d46728bf634f
David Wilcox <dave@davidgwilcox.com>
2014-01-21 03:55:18 +0000

    changed the services to point to bvn-production for deployment

--- client/app/scripts/services/person.js
+++ client/app/scripts/services/person.js
@@ -4,7 +4,7 @@ angular.module('ShadowWolf')
 .service('Person', function($resource, $http, Session) {
   $http.defaults.useXDomain = true;
   
-  var _Person = $resource('http://localhost:port/people/:personId.json?access_token=:accessToken',
+  var _Person = $resource('http://10.1.3.70:port/people/:personId.json?access_token=:accessToken',
     { port: ':3000', personId: '@personId', accessToken: Session.getAccessToken() });
   this.getPerson = function(personId) { return _Person.get({personId: personId}); };
 });
