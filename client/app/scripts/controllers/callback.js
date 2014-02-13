59c45be0f94ebc86ef82c79b77f838f0866e1e00
David Wilcox <dave@davidgwilcox.com>
2014-02-13 05:39:51 +0000

    fixes #121 :boar:

--- client/app/scripts/controllers/callback.js
+++ client/app/scripts/controllers/callback.js
@@ -3,9 +3,22 @@
 // The callback controler handles the login callback
 angular.module('ShadowWolf')
 .controller('CallbackController',
-function($scope, $location, Session) {
-  Session.setAccessToken($location.search().access_token);
-  Session.setPersonId($location.search().person_id);
+function($scope, $location, Session, Flash) {
+  var message = $location.search().message;
+  if (message) {
+    Flash.add({
+      template: '<p>You have failed to login: {{ flash.message }}</p>',
+      message: message,
+      css: 'flash-failure'
+    }, 5000);
+  } else {
+    Flash.add({
+      template: '<p>You have successfully logged in.</p>',
+      css: 'flash-success'
+    }, 5000);
+    Session.setAccessToken($location.search().access_token);
+    Session.setPersonId($location.search().person_id);
+  }
   $location.search('');
   $location.path("/people");
 });
--- client/app/scripts/services/flash.js
+++ client/app/scripts/services/flash.js
@@ -4,18 +4,25 @@ angular.module('ShadowWolf')
 .service('Flash', function($timeout, $compile) {
   var _flashes = [];
   this.get = function() { return _flashes; };
-  this.add = function(flash) {
+  this.add = function(flash, timeout) {
     var _that = this;
     _flashes.splice(0,0,flash);
 
-    return {
-      timeout: function(duration) {
-        $timeout( function() {
-          _that.dismiss( flash );
-        }, duration || 5000
-        );
-      }
-    };
+    if (timeout) {
+      $timeout( function() {
+        _that.dismiss( flash );
+      }, timeout || 5000
+      );
+    } else {
+      return {
+        timeout: function(duration) {
+          $timeout( function() {
+            _that.dismiss( flash );
+          }, duration || 5000
+          );
+        }
+      };
+    }
   };
   this.dismiss = function(flash) {
     var index = _flashes.indexOf(flash);
