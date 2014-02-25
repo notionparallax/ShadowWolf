ad6a3c6e67b2ddaced5751eaf0a58f0961f911e2
David Wilcox <dave@davidgwilcox.com>
2014-02-25 02:17:06 +0000

    modified gruntfile to play well with port forwarding

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
@@ -412,7 +412,7 @@ module.exports = function (grunt) {
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
-      'open',
+      //'open',
       'watch'
     ]);
   });
