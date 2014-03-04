f6303d7d632071238c57a10839c77060e9ca7171
David Wilcox <dave@davidgwilcox.com>
2014-03-04 00:43:28 +0000

    removed some obselete code from client controllers

--- client/app/scripts/controllers/people/show.js
+++ client/app/scripts/controllers/people/show.js
@@ -7,29 +7,4 @@ function($scope, Person, $routeParams, Session, $location, Lens) {
     $location.path("/people");
   }
   $scope.person = Person.getPerson($routeParams.personId);
-  $scope.updateObject = Person.update;
-
-  $scope.addObject = function(parentObject,property) {
-    parentObject[property].push({});
-  };
-  $scope.removeObject = function(lens,object) {
-    var yes = confirm("Are you sure you wish to delete this item?");
-    if (!yes) return;
-
-    var parentObject = Lens.get($scope.person, lens);
-    var index = parentObject.indexOf(object);
-    parentObject.splice(index,1);
-    var updateObject =
-      { person: Lens.wrapObject(lens, parentObject) }; 
-    console.log(updateObject);
-
-    $scope.updatePerson($scope.person.id['$oid'],
-      updateObject,
-      function(){
-      console.log("successfully destroyed");
-    }, function(){
-      console.log("unsuccessfully destroyed");
-      parentObject.push(object);
-    });
-  };
 });
