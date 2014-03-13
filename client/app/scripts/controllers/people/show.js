434821a5416cb1804d6f3e0d3f3b20afecfd40e7
David Wilcox <dave@davidgwilcox.com>
2014-03-13 00:22:12 +0000

    close #187, however it seems unusually slow.
    I think this is because there are 1933 projects being loaded and would go away
    if I showed less projects or I used the `track by` feature on the `ng-repeat`,
    I think that speeds it up.

--- client/app/scripts/controllers/people/show.js
+++ client/app/scripts/controllers/people/show.js
@@ -2,8 +2,12 @@
 
 angular.module('ShadowWolf')
 .controller('PeopleShowController',
-function($scope, Person, $routeParams, Session, $location, Lens) {
+function($scope, Person, $routeParams, Session, $location, Lens, Flash) {
   if (!Session.getPersonId()) {
+    Flash.add({
+      template: '<p>You must be logged in to view that page.</p>',
+      css: 'flash-fail'
+    }, 5000);
     $location.path("/people");
   }
   $scope._person = function() { return Person.get($routeParams.personId); };
