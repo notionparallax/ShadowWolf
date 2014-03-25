3f5a0bde3c972ae3d4063e94bfc8c618b64b2fc5
Ben Doherty <ben@notionparallax.co.uk>
2014-03-25 01:10:52 +0000

    made very basic maps show mostly @dawil doing the thinking

--- client/app/scripts/filters/id.js
+++ client/app/scripts/filters/id.js
@@ -6,3 +6,29 @@ angular.module('ShadowWolf')
     return (input.id ||{ '$oid':''})['$oid'];
   };
 });
+
+angular.module('ShadowWolf')
+.filter('trustMaplink',function($sce) {
+  return function(input) {
+  	window.x = $sce.trustAsUrl(input);
+    return $sce.trustAsUrl(input).$$unwrapTrustedValue();
+  };
+});
+
+
+
+angular.module('ShadowWolf').config(function($sceDelegateProvider) {
+ $sceDelegateProvider.resourceUrlWhitelist([
+   // Allow same origin resource loads.
+   'self',
+   // Allow loading from our assets domain.  Notice the difference between * and **.
+   'https://maps.google.com.au/**']);
+ });
+
+
+// ["https://maps.google.com.au/?ie=UTF8",
+//     "ll=" + subobject.lat + "," + subobject.lon,
+//     "spn=0.016839,0.031629",
+//     "t=h",
+//     "z=" + "9",
+//     "output=embed"].join("&amp;")
