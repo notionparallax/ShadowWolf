55dc50e0029a037b401482357a13297e49083524
David Wilcox <dave@davidgwilcox.com>
2014-04-08 18:03:32 +1000

    added a test which mocks an http get request

--- client/test/hello.js
+++ client/test/hello.js
@@ -8,6 +8,7 @@ describe('initial tests',function(){
       + path;
     return browser.get(url);
   }
+
   it('login button exists', function(){
     visit('people').then(function(){
       return element(by.css('.login-btn a'));
@@ -17,4 +18,34 @@ describe('initial tests',function(){
       expect(loginText).toBe('Log In');
     });
   });
+
+  describe('mocks data', function(){
+    var httpBackendMock = function(){
+      console.log("hellohello");
+      angular.module('httpBackendMock', ['ShadowWolf', 'ngMockE2E'])
+      .run(function($httpBackend) {
+        var person = { name: { 'preferred_last': 'gvhbjn' } };
+        $httpBackend.whenGET(/people.json/).respond([person]);
+        $httpBackend.whenGET(/.*/).passThrough();
+      });
+    };
+    ptor = protractor.getInstance();
+    ptor.addMockModule('httpBackendMock', httpBackendMock);
+
+    it('people are listed on /people', function(){
+      visit('people').then(function(){
+        return element.all(by.css('.test'));
+      }).then(function(rows){
+        console.log(rows);
+        rows[0].getInnerHtml().then(function(html){
+          console.log("async");
+          console.log(html);
+        });
+        expect(false);
+        expect(rows.length).toBeGreaterThan(0);
+      });
+    });
+  });
+
+
 });
