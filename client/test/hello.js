63b13a34fcfcaa565eb409c28bd6431d61094191
Ben <ben@notionparallax.co.uk>
2014-04-14 11:58:53 +1000

    test for #224
    I don't think either will work yet, but they are going in the right direction

--- client/test/hello.js
+++ client/test/hello.js
@@ -92,6 +92,7 @@ describe('initial tests:',function(){
   });
   
   describe('editable tests', function(){
+
     xit("should show help text when a label is hovered", function() {
       //Where helptext container is called `x`
       magicalEditableSetupFunction(someJSON, someHelpText, more, args, that, we, will, need);
@@ -99,6 +100,28 @@ describe('initial tests:',function(){
       expect($('#helptext-for-x')).toBeHidden();
       $('label.editable-lable"').trigger('mouseout');
       expect($('#helptext-for-x')).toBeShown();
+    });
+
+    xit("should save the text entered if ENTER is pressed", function() {
+      magicalEditableSetupFunction(someJSON, someHelpText, more, args, that, we, will, need);
+      magicalEditable_gotoEditMode('selector.of.editable.we.want').then(function(edInput){
+        edInput.clear();
+        edInput.sendKeys("I'm typing, pretty cool eh!")
+        ptor.actions().sendKeys(protractor.Key.ENTER).perform(); //http://stackoverflow.com/questions/19914915/how-to-make-protractor-press-the-enter-key
+      }).then(function(){
+        expect($('div.form-control-static.editable.editable-text')).toMatch("I'm typing, pretty cool eh!");
+      });
+      
+      xit("should NOT save the text entered if ENTER is pressed", function() {
+      magicalEditableSetupFunction(someJSON, someHelpText, more, args, that, we, will, need);
+      magicalEditable_gotoEditMode('selector.of.editable.we.want').then(function(edInput){
+        edInput.clear();
+        edInput.sendKeys("I'm typing, pretty cool eh!")
+        ptor.actions().sendKeys(protractor.Key.ESCAPE).perform(); //http://stackoverflow.com/questions/19914915/how-to-make-protractor-press-the-enter-key
+      }).then(function(){
+        expect($('div.form-control-static.editable.editable-text')).not.toMatch("I'm typing, pretty cool eh!");
+      });
+
     });
   });
 
