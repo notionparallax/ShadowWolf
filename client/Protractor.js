b8382bda97dd841b17e829ff1fe3520ae69e3a99
David Wilcox <dave@davidgwilcox.com>
2014-05-13 16:45:44 +1000

    initial progress on #251

--- client/Protractor.js
+++ client/Protractor.js
@@ -9,10 +9,11 @@ exports.config = {
   					+ '/wd/hub',
   //  specs: ['test/hello.js']
   suites: {
-    all: 'test/**/*.js',
+    all: 'test/e2e/**/*.js',
     index: 'test/e2e/index.js',
     show: 'test/e2e/show.js',
-    editables: 'test/e2e/editables.js'
+    editables: 'test/e2e/editables.js',
+    general: 'test/e2e/general.js'
   }
     //http://eitanp461.blogspot.com.au/2014/01/advanced-protractor-features.html
     //'loggingPrefs': {
--- client/test/utils.js
+++ client/test/utils.js
@@ -1,3 +1,5 @@
+var LoginButton = require("./page_objects.js").LoginButton;
+
 function setup(){
   afterEach(function(){
     ptor = protractor.getInstance();
@@ -13,30 +15,61 @@ function visit(path) {
     + path;
   return browser.get(url);
 }
-function mockBackend(mappings) {
-  /* This is pretty revolting. Dave made an issue here:
-     https://github.com/angular/protractor/issues/695*/
+function mockBackend(mappings, personId, accessToken) {
+  /**
+   * This is pretty revolting. Dave made an issue here:
+   *   https://github.com/angular/protractor/issues/695
+   * See a PR to fix here:
+   *   https://github.com/angular/protractor/pull/787
+   */
+      
   var httpBackendMock = function(){
     angular.module('httpBackendMock', ['ShadowWolf', 'ngMockE2E'])
     .run(function($httpBackend) {
       // UGLY
       for (var regex in mappings) {
         $httpBackend.whenGET(new RegExp(regex))
-          .respond(mappings[regex]);
+          .respond(function(){
+            if (personId && accessToken) {
+              // check for presence of accessToken
+              // check that personId matches
+              // gives 3 tiers of permissions
+            } else {
+              return mappings[regex];
+            }
+          });
       }
+  //    if (personId) {
+  //      $httpBackend.whenGET(new RegExp('/people/auth/'))
+  //        .respond(function(){
+  //          return [303
+  //                 ,'access_token=test&person_id=' + personId
+  //                 ,{'Location': '/#/callback'}];
+  //        });
+  //    }
       $httpBackend.whenGET(/.*/).passThrough();
     });
   };
 
   var httpBackendMockString = '(' + httpBackendMock.toString()
-    .replace('// UGLY', 'var mappings = ' + JSON.stringify(mappings) + ';')
+    .replace('// UGLY', 'var mappings = ' + JSON.stringify(mappings) + ';'
+                      + 'var personId = ' + JSON.stringify(personId) + ';'
+                      + 'var accessToken = ' + JSON.stringify(accessToken) + ';')
     .replace(/\s+/g,' ') + '())';
   ptor = protractor.getInstance();
   ptor.addMockModule('httpBackendMock', httpBackendMockString);
+}
+function logIn(personId){
+  // 1. mock the backend to intercept login request
+  // 2. click on login button
+  visit('callback?person_id=' + personId + '&access_token=test-token');
+  // 3. get redirected to home page with successful login and access token in session service
+  // 
 }
 
 module.exports = {
   setup: setup,
   visit: visit,
-  mockBackend: mockBackend
+  mockBackend: mockBackend,
+  logIn: logIn
 }
