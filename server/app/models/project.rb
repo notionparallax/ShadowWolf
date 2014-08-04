3a9354f9055d9743cad807096d59e6d11c3b6eb2
David Wilcox <dave@davidgwilcox.com>
2014-08-04 16:08:20 +1000

    new projects get fully stubbed out, helping #321

--- server/app/models/project.rb
+++ server/app/models/project.rb
@@ -13,4 +13,40 @@ class Project
   field :project_number, type: String
   field :img, type: String
   field :value, type: String
+
+  before_create :default_subobjects
+
+  private
+  def default_subobjects
+    self.non_building                                   =   NonBuilding.new              if  self.non_building.nil?
+    self.building                                       =   Building.new                 if  self.building.nil?
+    self.building.client                                =   Client.new                   if  self.building.client.nil?
+    self.building.project_source                        =   ProjectSource.new            if  self.building.project_source.nil?
+    self.building.project_source.referred_by            =   ReferredBy.new               if  self.building.project_source.referred_by.nil?
+    self.building.legacy                                =   Legacy.new                   if  self.building.legacy.nil?
+    self.building.legacy.awards                         <<  Award.new                    if  self.building.legacy.awards.length                         ==  0
+    self.building.legacy.esd                            =  EnvironmentalSustainableDesign.new                      if  self.building.legacy.esd.nil?
+    self.building.legacy.esd.certifications          <<  Certification.new            if  self.building.legacy.esd.certifications.length          ==  0
+    self.building.legacy.esd.initiatives             <<  Initiative.new              if  self.building.legacy.esd.initiatives.length             ==  0
+    self.building.legacy.quotes                         <<  Quote.new                   if  self.building.legacy.quotes.length                         ==  0
+    self.building.legacy.testimonials                   <<  Testimonial.new             if  self.building.legacy.testimonials.length                   ==  0
+    self.building.relationship_description              =   RelationshipDescription.new  if  self.building.relationship_description.nil?
+    self.building.dims                                  =   DimensionGroup.new           if  self.building.dims.nil?
+    self.building.dims.footprint_area                   =   FootprintArea.new            if  self.building.dims.footprint_area.nil?
+    self.building.dims.gba                              =   GrossBuildingArea.new                      if  self.building.dims.gba.nil?
+    self.building.dims.gfa                              =   GrossFloorArea.new                      if  self.building.dims.gfa.nil?
+    self.building.dims.height                           =   Height.new                   if  self.building.dims.height.nil?
+    self.building.dims.nla                              =   NetLettableArea.new                      if  self.building.dims.nla.nil?
+    self.building.qas                                   <<  QualityAssurance.new         if  self.building.qas.length                                   ==  0
+    self.building.locations                             <<  Location.new                 if  self.building.locations.length                             ==  0
+    self.building.locations[0].address                  =   Address.new                  if  self.building.locations[0].address.nil?
+    self.building.locations[0].geo_json                 =   GeoJson.new                  if  self.building.locations[0].geo_json.nil?
+    self.building.phases                                <<  Phase.new                    if  self.building.phases.length                                ==  0
+    self.building.phases[0].budget                      =   Budget.new                   if  self.building.phases[0].budget.nil?
+    self.building.phases[0].key_dates                   <<  KeyDate.new                  if  self.building.phases[0].key_dates.length                   ==  0
+    self.building.presses                               <<  BuildingPress.new                    if  self.building.presses.length                               ==  0
+    self.building.presses[0].attentions                 <<  Attention.new                if  self.building.presses[0].attentions.length                 ==  0
+    self.building.presses[0].campaigns                  <<  Campaign.new                 if  self.building.presses[0].campaigns.length                  ==  0
+    
+  end
 end
