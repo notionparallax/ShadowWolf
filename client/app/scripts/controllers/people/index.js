7c968647363e20a582fe028f0100bdf184de63ee
Ben Doherty <ben@notionparallax.co.uk>
2014-08-27 17:11:35 +1000

    stormtroopers closed #309

--- client/app/scripts/controllers/people/index.js
+++ client/app/scripts/controllers/people/index.js
@@ -4,11 +4,32 @@ angular.module('ShadowWolf')
 .controller('PeopleIndexController',
 function($scope, People, Session, Search, GA) {
   $scope.getCurrentUserLogin = function() { return Session.getPersonLogin(); };
-  $scope.searchResults = [];
+  function getDefaultSearchResults() {
+    var  results = [];
+    for (var i = 0; i < 30; i++) {
+      results[i] = {
+        name: { preferred_first: '', preferred_last: '' },
+        employee:
+          {
+            stormtrooperClass: 'stormtrooper',
+            contact: { extension: '' },
+            photo: { bw: 'images/fb_stormtrooper.jpg'},
+            current_condition_name: 'Active',
+            id: { $oid: '' }
+          }
+      };
+    }
+    return results;
+  }
+  $scope.searchResults = getDefaultSearchResults();
+
+
   People.getPeople().$promise.then(function(people) {
     $scope.searchResults = people;
   });
+
   $scope.$watch('searchQuery', function(newValue) {
+    if ($scope.searchQuery === undefined) return;
     $scope.searchResults = People.getPeople().filter($scope.compareTo(newValue));
   });
 
