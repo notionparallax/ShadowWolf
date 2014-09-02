dcd0601328f66c8201802e7fec0813e0c159497c
David Wilcox <dave@davidgwilcox.com>
2014-09-02 16:46:45 +1000

    wrote most of the feature files, editables to come

---
+++ client/test/features/people/people_index.feature
@@ -0,0 +1,12 @@
+Feature: People Index
+
+  Scenario: Visiting a people page
+    Given I am on the people index
+    When  I click on a person
+    Then  I should be redirected to the person's summary page
+
+  Scenario:
+    Given I am on the people index
+    And   the database contains certain people
+    When  I type into the search bar
+    Then  the displayed people should change accordingly
--- client/test/features/support/world.js
+++ client/test/features/support/world.js
@@ -1,7 +1,7 @@
-var browser = require('protractor').getInstance();
-var World = function(callback) {
-  this.browser = browser;
-  this.x = require('protractor');
-  callback();
-};
-exports.World = World;
+//var browser = require('protractor').getInstance();
+//var World = function(callback) {
+//  this.browser = browser;
+//  this.x = require('protractor');
+//  callback();
+//};
+//exports.World = World;
