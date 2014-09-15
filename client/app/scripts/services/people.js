7868194804d540b1992ef7e21bed09713b8b9117
David Wilcox <dave@davidgwilcox.com>
2014-09-15 18:25:14 +1000

    prelim work on #348 using localStorage

--- client/app/scripts/services/people.js
+++ client/app/scripts/services/people.js
@@ -3,8 +3,27 @@
 angular.module('ShadowWolf')
 .service('People', function($resource, $http, Session,Config) {
   $http.defaults.useXDomain = true;
+  function initPeople() {
+    var peoplePromise = _People.query();
+    peoplePromise.$promise.then(function(people) {
+      localStorage.people = angular.toJson(people);
+    });
+    if (localStorage.people) {
+      console.log('a');
+      return { $promise:
+        { then: function(handler) {
+            return handler(angular.fromJson(localStorage.people));
+          }
+        }
+      };
+    } else {
+      console.log('b');
+      return peoplePromise;
+    }
+  }
+
   var _People = $resource(Config.getEndPoint() + '/people.json');
-  var _people = _People.query();
+  var _people = initPeople();
   this.getPeople = function() { return _people; };
   this.getPerson = function(id) {
     for (var p in _people) {
