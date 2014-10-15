1776cffc84f706621a2e01ade777c85c446e2235
David Wilcox <dave@davidgwilcox.com>
2014-10-15 18:41:32 +1100

    further progress on adding in fig. Now builds most images

--- client/run.sh
+++ client/run.sh
@@ -2,11 +2,12 @@
 
 ENDPOINT="http://$SERVER_PORT_3000_TCP_ADDR:$SERVER_PORT_3000_TCP_PORT"
 RETURNPOINT="http://$SERVER_PORT_3000_TCP_ADDR:9000"
-cat << EOF > dist/scripts/endpoint.js
+cat << EOF > /dist/scripts/endpoint.js
 'use strict';
 angular.module('ShadowWolf')
 .constant('ENDPOINT', "$ENDPOINT")
 .constant('RETURNPOINT', "$RETURNPOINT");
 EOF
-sed -ibak 's@</body>@<script src="scripts/endpoint.js"></script></body>@' dist/index.html
-exec grunt connect:dist:keepalive
+sed -ibak 's@</body>@<script src="scripts/endpoint.js"></script></body>@' /dist/index.html
+cd /dist
+exec python3 -m http.server 9000
