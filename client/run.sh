e3fef9af844f908f1c22928d427861f43823ec56
David Wilcox <dave@davidgwilcox.com>
2014-12-16 18:16:34 +1100

    update client/run.sh for production deployments of oaf

--- client/run.sh
+++ client/run.sh
@@ -3,11 +3,13 @@
 ENDPOINT="http://$SERVER_PORT_3000_TCP_ADDR:$SERVER_PORT_3000_TCP_PORT"
 RETURNPOINT="http://$SERVER_PORT_3000_TCP_ADDR:9000"
 BEOWULF_SERVER="http://$SERVER_PORT_3000_TCP_ADDR:$BEOWULF_PORT_9001_TCP_PORT"
+BEOWULF_SERVER="http://$OAF_PORT_4567_TCP_ADDR:$OAF_PORT_4567_TCP_PORT"
 cat << EOF > /dist/scripts/endpoint.js
 'use strict';
 angular.module('ShadowWolf')
 .constant('ENDPOINT', "$ENDPOINT")
 .constant('RETURNPOINT', "$RETURNPOINT")
+.constant('OAF_SERVER', "$OAF_SERVER")
 .constant('BEOWULF_SERVER', "$BEOWULF_SERVER");
 EOF
 sed -ibak 's@</body>@<script src="scripts/endpoint.js"></script></body>@' /dist/index.html
