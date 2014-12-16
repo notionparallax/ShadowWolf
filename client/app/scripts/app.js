317e390a29001e2a2546b2497d6e2fb543041a9c
David Wilcox <dave@davidgwilcox.com>
2014-12-16 17:41:29 +1100

    tell bootstrap-angular's tabset to use ng-if
    This is a hefty performance improvement for the project's show page by
    reducing the size of the scope object.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,4 +1,20 @@
 'use strict';
+angular.module("template/tabs/tabset.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("template/tabs/tabset.html",
+    "\n" +
+    "<div class=\"tabbable\">\n" +
+    "  <ul class=\"nav {{type && 'nav-' + type}}\" ng-class=\"{'nav-stacked': vertical, 'nav-justified': justified}\" ng-transclude></ul>\n" +
+    "  <div class=\"tab-content\">\n" +
+    "    <div class=\"tab-pane\" \n" +
+    "         ng-repeat=\"tab in tabs\" \n" +
+    "         ng-if=\"tab.active\" \n" +
+    "         ng-class=\"{active: tab.active}\"\n" +
+    "         tab-content-transclude=\"tab\">\n" +
+    "    </div>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
+}]);
 
 angular.module('ShadowWolf', ['ngResource',
                               'ngRoute',
