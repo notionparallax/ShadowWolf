0eec538d0da4c4e694b62ae06357d7465c21c2fb
David Wilcox <dave@davidgwilcox.com>
2014-12-17 18:05:08 +1100

    got oaf to return the top 3 images instead of just 1

--- oaf/main.rb
+++ oaf/main.rb
@@ -22,7 +22,7 @@ before do
     'Access-Control-Allow-Headers' => 'Content-Type'
 end
 
-def get_project_image_url project_number
+def get_project_image_urls project_number
   projects = settings.projects
   files = settings.files
   # get via oa image url
@@ -31,35 +31,37 @@ def get_project_image_url project_number
   oa_id  = project['id']
   images = JSON.parse(    files.get( accept: :json, params: { textMatching: 'exact', project_id: oa_id             } ) )
   return nil if images.empty?
-  first_image_sizes = JSON.parse( files["/#{images.first['id']}/Sizes"].get( accept: :json, params: { file_format: 'jpg' } ) )
-  image_closest_to_200_width = first_image_sizes.min { |a,b| (200-a['width'].to_i).abs <=> (200-b['width'].to_i).abs }
+  first_3_image_urls = images.sort { |a,b| b['rank'].to_i <=> a['rank'].to_i }[0..2].map do |image|
+    image_sizes = JSON.parse( files["/#{image['id']}/Sizes"].get( accept: :json, params: { file_format: 'jpg' } ) )
+    image_closest_to_1500_width = image_sizes.min { |a,b| (1500-a['width'].to_i).abs <=> (1500-b['width'].to_i).abs }
+    # construct image url
+    [ 'http://',
+      ENV['OPEN_ASSET_ENDPOINT'],
+      image_closest_to_1500_width['http_root'],
+      image_closest_to_1500_width['relative_path']
+    ].join
+  end
   
-  # construct image url
-  image_url = ['http://',
-    ENV['OPEN_ASSET_ENDPOINT'],
-    image_closest_to_200_width['http_root'],
-    image_closest_to_200_width['relative_path']
-  ].join
   # cache image url
-  settings.redis.set project_number, image_url
-  puts "set #{project_number} to have url #{image_url}"
+  settings.redis.set project_number, JSON.generate( first_3_image_urls )
   # return image url 
-  image_url
+  first_3_image_urls
 end
 
 options '/projects' do
   200
 end
 put '/projects' do
-  puts params
   project_numbers = JSON.parse request.body.read
   project_image_urls = {}
   project_numbers.each do |project_number|
-    image_url = settings.redis.get project_number
-    if image_url.nil? and params[:update_cache].eql? 'true'
-      image_url = get_project_image_url project_number
+    image_urls_as_json = settings.redis.get project_number
+    if image_urls_as_json.nil? and params[:update_cache].eql? 'true'
+      image_urls = get_project_image_urls project_number
+    elsif not image_urls_as_json.nil?
+      image_urls = JSON.parse image_urls_as_json
     end
-    project_image_urls[project_number] = image_url if image_url
+    project_image_urls[project_number] = image_urls if image_urls
   end
   project_image_urls.to_json
 end
