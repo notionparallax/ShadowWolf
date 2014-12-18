84b805dba13ff6f3c760c71634e659ef6facffbe
Ben Doherty <ben@notionparallax.co.uk>
2014-12-18 17:19:31 +1100

    make factories a bit nicer

--- server/spec/factories/phases.rb
+++ server/spec/factories/phases.rb
@@ -6,13 +6,13 @@ FactoryGirl.define do
     after(:build) do |phase|
       FactoryGirl.build_list( :key_date, [0,1,2,3,10].sample, phase: phase )
     end
-    dirty_members { [Faker::Name.first_name,Faker::Name.first_name,Faker::Name.first_name].join ', ' }
-    project_name { Faker::Commerce.product_name + " " + ["base", "tower", "building", "Labs"].sample}
-    studio { ['Brisbane', 'Sydney', 'Canberra', 'Auckland', 'Bangkok', 'London', 'Melbourne', 'Townsville'].sample }
-    client { Faker::Company.name }
-    services_scope { Faker::Lorem.paragraphs.join('<br>') }
-    delivery_contract { Faker::Lorem.paragraphs.join('<br>') }
-    staff_contact_login { (Faker::Base::ULetters.sample + Faker::Name.last_name ).downcase }
+    dirty_members         { [Faker::Name.first_name,Faker::Name.first_name,Faker::Name.first_name].join ', ' }
+    project_name          { Faker::Commerce.product_name + " " + ["base", "tower", "building", "Labs"].sample}
+    studio                { ['Brisbane', 'Sydney', 'Canberra', 'Auckland', 'Bangkok', 'London', 'Melbourne', 'Townsville'].sample }
+    client                { Faker::Company.name }
+    services_scope        { Faker::Lorem.paragraph }
+    delivery_contract     { Faker::Lorem.paragraphs.join('<br>') }
+    staff_contact_login   { (Faker::Base::ULetters.sample + Faker::Name.last_name ).downcase }
     staff_custodian_login { (Faker::Base::ULetters.sample + Faker::Name.last_name ).downcase }
     dirty_date "Projected completion 2005"
     referee { "Pierluigi Collina" }
