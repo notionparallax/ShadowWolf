7e59affef4506c829bb07c4f1ae52264a245e72b
Ben Doherty <ben@notionparallax.co.uk>
2015-01-15 15:32:44 +1100

    changed jury to jury citation, closes #450

--- server/spec/factories/awards.rb
+++ server/spec/factories/awards.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :award do
     award { Faker::Company.catch_phrase }
     date  { (0..20).to_a.sample.years.ago + (1..345).to_a.sample.days }
-    jury  { [Faker::Name.name,Faker::Name.name,Faker::Name.name ,Faker::Name.name] }
+    jury  { Faker::Lorem.paragraphs.join('<br>') }
     tags  { [%w(retail business residential).sample, %w(education health finance).sample] }
   end
 end
