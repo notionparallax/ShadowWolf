0d132b48683116eb83f7d11efe45114242f2463c
David Wilcox <dave@davidgwilcox.com>
2015-02-13 12:22:48 +1100

    fixed a type error in awards factory

--- server/spec/factories/awards.rb
+++ server/spec/factories/awards.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :award do
     award { Faker::Company.catch_phrase }
     date  { (0..20).to_a.sample.years.ago + (1..345).to_a.sample.days }
-    jury  { Faker::Lorem.paragraphs.join('<br>') }
+    jury  { Faker::Lorem.paragraphs }
     tags  { [%w(retail business residential).sample, %w(education health finance).sample] }
   end
 end
