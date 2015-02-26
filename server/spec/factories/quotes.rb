4f584773bd1e114fdd39d4f99d9f486d15b16780
Ben <ben@notionparallax.co.uk>
2015-02-26 17:00:40 +1100

    added main to these files

--- server/spec/factories/quotes.rb
+++ server/spec/factories/quotes.rb
@@ -6,6 +6,6 @@ FactoryGirl.define do
     date     { (0..20).to_a.sample.years.ago + (1..345).to_a.sample.days }
     citation { Faker::Name.name  }
     context  { Faker::Name.name  + " " + ["Review", "Times"].sample}
-    tags     { [%w(retail business residential).sample, %w(education health finance).sample] }
+    tags { ["main", %w(retail business residential).sample, %w(education health finance).sample] }
   end
 end
