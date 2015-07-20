056f38f086cced34a5419dcf36b4face31147e0d
David Wilcox <dave@davidgwilcox.com>
2015-07-20 17:14:42 +1000

    added unit tests for oaf core

---
+++ oaf/tests/core.rb
@@ -0,0 +1,40 @@
+require 'test/unit'
+require 'mocha/test_unit'
+require './app/core.rb'
+
+class Example < Model
+  def self.from_hash hash
+    e = Example.new
+    e.oa_id = hash['id']
+    e
+  end
+  def to_json options
+    { id: @oa_id, example: true }
+  end
+end
+ 
+class TestCore < Test::Unit::TestCase
+
+  def setup
+    @model = Example.new
+    @data_source = DataSource.new
+    @model.data_source( @data_source )
+  end
+  def teardown
+  end
+ 
+  def test_data_source_from_json
+    e = @data_source.from_json :example, '{"id":5}'
+    assert_equal e.oa_id, 5
+  end
+ 
+  def test_model_get
+    @model = Example.new
+    @model.oa_id = 5
+    @model.data_source( @data_source )
+    @data_source.expects(:get)
+      .with( example: 5 )
+      .returns( id: 5 )
+    assert_equal @model.get( :example ), { id: 5 }
+  end
+end
