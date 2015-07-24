cdf52b5901bff5bbae597d0b0fd652675f6c5648
David Wilcox <dave@davidgwilcox.com>
2015-07-24 13:33:27 +1000

    added tests for oaf controller

--- oaf/Dockerfile
+++ oaf/Dockerfile
@@ -4,7 +4,7 @@ RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C3173AA6 && \
 RUN apt-get update && apt-get install -y ruby2.1 ruby2.1-dev build-essential \
   openssl libreadline6 libreadline6-dev git-core libyaml-dev libssl-dev zlib1g \
   redis-server zlib1g-dev
-RUN gem2.1 install sinatra rake bundler rest-client redis mocha --no-ri --no-rdoc
+RUN gem2.1 install sinatra rake bundler rest-client redis mocha rack rack-test --no-ri --no-rdoc
 ENV LANG en_AU.utf8
 ENV LANGUAGE en_AU.UTF-8
 ENV LC_ALL en_AU.UTF-8
---
+++ oaf/tests/controller.rb
@@ -0,0 +1,102 @@
+require 'test/unit'
+require 'mocha/test_unit'
+require 'rack/test'
+require './app/controller.rb'
+
+class ControllerTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    OAF
+  end
+
+  def setup
+    app.set :data_source, Object.new
+  end
+
+  def test_redirect_303s_on_success
+    size    = Object.new ;    size.stubs(:url => 'foo')
+    image   = Object.new ;   image.stubs(:[] => size)
+    project = Object.new ; project.stubs(images_by_tag: [image])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/0/web_view'
+
+    assert_equal 303, last_response.status
+    assert_equal 'foo', last_response.headers['Location']
+  end
+  def test_redirect_404s_on_no_project
+    app.settings.data_source.stubs(:get).with(project: '1').returns(nil)
+
+    get '/1/main/0/web_view'
+
+    assert_equal 404, last_response.status
+    assert_equal 'No project found with project number: "1".', last_response.body
+  end
+  def test_redirect_404s_on_no_images
+    project = Object.new ; project.stubs(images_by_tag: [])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/0/web_view'
+
+    assert_equal 404, last_response.status
+    assert_equal 'No images found with tag: "main".', last_response.body
+  end
+  def test_redirect_400s_on_negative_integer_index
+    project = Object.new ; project.stubs(images_by_tag: [])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/-1/web_view'
+
+    assert_equal 400, last_response.status
+    assert_equal 'Image index must be positive integer (-1 provided).', last_response.body
+  end
+  def test_redirect_404s_invalid_index
+    project = Object.new ; project.stubs(images_by_tag: [nil])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/1/web_view'
+
+    assert_equal 404, last_response.status
+    assert_equal 'No image at index: 1.', last_response.body
+  end
+  def test_redirect_400s_on_invalid_size
+    image   = Object.new ;   image.stubs(:[] => nil)
+    project = Object.new ; project.stubs(images_by_tag: [image])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/0/something_improper'
+
+    assert_equal 400, last_response.status
+    assert_equal 'Size must be one of: square, thumbnail, small, web_view, medium.', last_response.body
+  end
+  def test_redirect_404s_on_absent_size
+    image   = Object.new ;   image.stubs(:[] => nil)
+    project = Object.new ; project.stubs(images_by_tag: [image])
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1/main/0/web_view'
+
+    assert_equal 404, last_response.status
+    assert_equal 'The requested size was not available.', last_response.body
+  end
+
+  def test_json_200_json_on_success
+    project = Object.new ; project.stubs(images_by_tag: [])
+    project.stubs(to_json: 'foo')
+    app.settings.data_source.stubs(:get).with(project: '1').returns(project)
+
+    get '/1.json'
+
+    assert_equal   200, last_response.status
+    assert_equal 'foo', last_response.body
+  end
+  def test_json_404s_on_no_project
+    app.settings.data_source.stubs(:get).with(project: '1').returns(nil)
+
+    get '/1.json'
+
+    assert_equal    404, last_response.status
+    assert_equal 'null', last_response.body
+  end
+end
